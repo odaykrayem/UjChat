d209a3ad0a7821602fc2694328bba0862a472607
odaykrayem <0day.krayem0@gmail.com>
2022-03-12 12:49:37 +0200

    third: most of designs

--- app/src/main/java/com/example/ujchat/SplashActivity.java
+++ app/src/main/java/com/example/ujchat/SplashActivity.java
@@ -15,7 +15,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_activity);
+        setContentView(R.layout.activity_splash_first);
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
